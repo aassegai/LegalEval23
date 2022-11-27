c1c829766763089bee20e9edd7b4fa97e2c9d148
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2022-11-27 10:16:14 +0000

    remove tokenization from dataset

--- src/datasets/classification_dataset.py
+++ src/datasets/classification_dataset.py
@@ -4,30 +4,22 @@ import pandas as pd
 
 
 class ClassificationDataset(Dataset):
-    def __init__(self, dataframe, tokenizer, max_len):
-        self.data = dataframe
-        self.text = dataframe.text
-        self.tokenizer = tokenizer
-        self.targets = self.data.labels
+    def __init__(self, tok_texts, labels, max_len):
+        # self.data = dataframe
+        self.texts = tok_texts
+        # self.tokenizer = tokenizer
+        self.targets = labels
         self.max_len = max_len
 
     def __len__(self):
-        return len(self.text)
+        return len(self.targets)
 
     def __getitem__(self, index):
-        inputs = self.tokenizer(
-            self.text[index],
-            None,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            pad_to_max_length=True,
-            return_token_type_ids=True
-        )
-        ids = inputs['input_ids']
-        mask = inputs['attention_mask']
+        ids = self.texts['input_ids'][index]
+        mask = self.texts['attention_mask'][index]
 
         return {
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
-            'targets': torch.tensor(self.targets, dtype=torch.float)
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
         }
