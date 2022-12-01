b759ba8dded2c41ba76961f05a24748f452832de
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2022-12-01 09:39:12 +0000

    fix FFN dimensionality

--- src/model/classification/bert_classificator.py
+++ src/model/classification/bert_classificator.py
@@ -4,9 +4,9 @@ class BERTClassificator(torch.nn.Module):
     def __init__(self, model, num_labels=13, emb_size=768, dropout=0.3):
         super(BERTClassificator, self).__init__()
         self.l1 = model
-        self.pre_classifier = torch.nn.Linear(emb_size, emb_size/2)
+        self.pre_classifier = torch.nn.Linear(emb_size, emb_size)
         self.dropout = torch.nn.Dropout(dropout)
-        self.classifier = torch.nn.Linear(emb_size/2, num_labels)
+        self.classifier = torch.nn.Linear(emb_size, num_labels)
 
     def forward(self, input_ids, attention_mask):
         output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask)
