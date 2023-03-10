2b4c79d397437b8de7fcf834b9e9f0af421e902d
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2023-03-10 05:19:02 +0000

    fix reshape problem

--- src/preprocessing/embedder.py
+++ src/preprocessing/embedder.py
@@ -3,13 +3,14 @@ import numpy as np
 import torch
 
 
-def embed(tok_texts, embedder, max_len=128, emb_size=512, batch_size=32):
+def embed(tok_texts, embedder, max_len=128, emb_size=512, batch_size=32, device='cpu'):
     embedded = []
+    embedder.to(device=device)
     for batch_start in tqdm(range(0, len(tok_texts['input_ids']), batch_size)):
-        batch_output = embedder(input_ids=tok_texts['input_ids'][batch_start : batch_start + batch_size], 
-                          attention_mask=tok_texts['attention_mask'][batch_start : batch_start + batch_size])
+        batch_output = embedder(input_ids=tok_texts['input_ids'][batch_start : batch_start + batch_size].to(device=device), 
+                          attention_mask=tok_texts['attention_mask'][batch_start : batch_start + batch_size].to(device=device))
         # print(batch_output)
         hidden_state = batch_output['last_hidden_state'].detach().cpu().numpy()
         # print(hidden_state)
-        embedded.append(hidden_state)
-    return np.array(embedded).reshape(len(tok_texts['input_ids']), max_len, emb_size)
+        embedded.extend(hidden_state)
+    return np.array(embedded)
