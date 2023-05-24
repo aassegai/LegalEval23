cd79e4fddcf28713970e4c679908e864b1dc0a1a
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2023-05-24 08:44:33 +0000

    fix histogram plot

--- utils.py
+++ utils.py
@@ -73,8 +73,9 @@ def aggregate_label_stats(df: pd.DataFrame):
 
 def display_label_counts(df: pd.DataFrame):
 
-    fig, ax = plt.subplots(1, 1, figsize=(20, 5))
+    fig, ax = plt.subplots(1, 1, figsize=(22, 5))
     labels_list = df['label'].to_list()
     for label in sorted(set(labels_list)):
-        plt.hist([label_item for label_item in labels_list if label_item == label], bins=np.arange(14)-0.5, rwidth=0.5)
+        plt.hist([label_item for label_item in labels_list if label_item == label], 
+        bins=np.arange(14)-0.5, rwidth=0.8)
     plt.show()
