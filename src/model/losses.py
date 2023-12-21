f2df42555484d6d2411ff2f6e9a7815bb60d60d1
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2023-12-21 05:04:55 +0000

    add L-tag

---
+++ src/model/losses.py
@@ -0,0 +1,282 @@
+import torch
+from torch import nn
+from typing import Optional
+from torch import Tensor
+import torch.functional as F
+
+
+class DiceLoss(nn.Module):
+    """
+    Dice coefficient for short, is an F1-oriented statistic used to gauge the similarity of two sets.
+    Given two sets A and B, the vanilla dice coefficient between them is given as follows:
+        Dice(A, B)  = 2 * True_Positive / (2 * True_Positive + False_Positive + False_Negative)
+                    = 2 * |A and B| / (|A| + |B|)
+    Math Function:
+        U-NET: https://arxiv.org/abs/1505.04597.pdf
+        dice_loss(p, y) = 1 - numerator / denominator
+            numerator = 2 * \sum_{1}^{t} p_i * y_i + smooth
+            denominator = \sum_{1}^{t} p_i + \sum_{1} ^{t} y_i + smooth
+        if square_denominator is True, the denominator is \sum_{1}^{t} (p_i ** 2) + \sum_{1} ^{t} (y_i ** 2) + smooth
+        V-NET: https://arxiv.org/abs/1606.04797.pdf
+    Args:
+        smooth (float, optional): a manual smooth value for numerator and denominator.
+        square_denominator (bool, optional): [True, False], specifies whether to square the denominator in the loss function.
+        with_logits (bool, optional): [True, False], specifies whether the input tensor is normalized by Sigmoid/Softmax funcs.
+        ohem_ratio: max ratio of positive/negative, defautls to 0.0, which means no ohem.
+        alpha: dsc alpha
+    Shape:
+        - input: (*)
+        - target: (*)
+        - mask: (*) 0,1 mask for the input sequence.
+        - Output: Scalar loss
+    Examples:
+        >>> loss = DiceLoss(with_logits=True, ohem_ratio=0.1)
+        >>> input = torch.FloatTensor([2, 1, 2, 2, 1])
+        >>> input.requires_grad=True
+        >>> target = torch.LongTensor([0, 1, 0, 0, 0])
+        >>> output = loss(input, target)
+        >>> output.backward()
+    """
+    # TODO: ohem ratio is not checked
+    def __init__(self,
+                 smooth: Optional[float] = 1e-4,
+                 square_denominator: Optional[bool] = False,
+                 with_logits: Optional[bool] = True,
+                 ohem_ratio: float = 0.0,
+                 alpha: float = 0.0,
+                 reduction: Optional[str] = "mean",
+                 index_label_position=True) -> None:
+        super(DiceLoss, self).__init__()
+
+        self.reduction = reduction
+        self.with_logits = with_logits
+        self.smooth = smooth
+        self.square_denominator = square_denominator
+        self.ohem_ratio = ohem_ratio
+        self.alpha = alpha
+        self.index_label_position = index_label_position
+
+    def forward(self, input: Tensor, target: Tensor, mask: Optional[Tensor] = None) -> Tensor:
+        logits_size = input.shape[-1]
+
+        if logits_size != 1:
+            loss = self._multiple_class(input, target, logits_size, mask=mask)
+        else:
+            loss = self._binary_class(input, target, mask=mask)
+
+        if self.reduction == "mean":
+            return loss.mean()
+        if self.reduction == "sum":
+            return loss.sum()
+        return loss
+
+    def _compute_dice_loss(self, flat_input, flat_target):
+        flat_input = ((1 - flat_input) ** self.alpha) * flat_input
+        interection = torch.sum(flat_input * flat_target, -1)
+        if not self.square_denominator:
+            loss = 1 - ((2 * interection + self.smooth) /
+                        (flat_input.sum() + flat_target.sum() + self.smooth))
+        else:
+            loss = 1 - ((2 * interection + self.smooth) /
+                        (torch.sum(torch.square(flat_input, ), -1) + torch.sum(torch.square(flat_target), -1) + self.smooth))
+
+        return loss
+
+    def _multiple_class(self, input, target, logits_size, mask=None):
+        flat_input = input
+        flat_target = F.one_hot(target, num_classes=logits_size).float() if self.index_label_position else target.float()
+        flat_input = torch.nn.Softmax(dim=1)(flat_input) if self.with_logits else flat_input
+
+        if mask is not None:
+            mask = mask.float()
+            flat_input = flat_input * mask
+            flat_target = flat_target * mask
+        else:
+            mask = torch.ones_like(target)
+        loss = None
+        if self.ohem_ratio > 0 :
+            mask_neg = torch.logical_not(mask)
+            for label_idx in range(logits_size):
+                pos_example = target == label_idx
+                neg_example = target != label_idx
+
+                pos_num = pos_example.sum()
+                neg_num = mask.sum() - (pos_num - (mask_neg & pos_example).sum())
+                keep_num = min(int(pos_num * self.ohem_ratio / logits_size), neg_num)
+
+                if keep_num > 0:
+                    neg_scores = torch.masked_select(flat_input, neg_example.view(-1, 1).bool()).view(-1, logits_size)
+                    neg_scores_idx = neg_scores[:, label_idx]
+                    neg_scores_sort, _ = torch.sort(neg_scores_idx, )
+                    threshold = neg_scores_sort[-keep_num + 1]
+                    cond = (torch.argmax(flat_input, dim=1) == label_idx & flat_input[:, label_idx] >= threshold) | pos_example.view(-1)
+                    ohem_mask_idx = torch.where(cond, 1, 0)
+
+                    flat_input_idx = flat_input[:, label_idx]
+                    flat_target_idx = flat_target[:, label_idx]
+
+                    flat_input_idx = flat_input_idx * ohem_mask_idx
+                    flat_target_idx = flat_target_idx * ohem_mask_idx
+                else:
+                    flat_input_idx = flat_input[:, label_idx]
+                    flat_target_idx = flat_target[:, label_idx]
+
+                loss_idx = self._compute_dice_loss(flat_input_idx.view(-1, 1), flat_target_idx.view(-1, 1))
+                if loss is None:
+                    loss = loss_idx
+                else:
+                    loss += loss_idx
+            return loss
+
+        else:
+            input_soft = ((1 - flat_input) ** self.alpha) * flat_input
+            intersection = torch.sum(input_soft * flat_target, dim=-1)
+            cardinality = torch.sum(input_soft + flat_target, dim=-1)
+            dice_score = (2. * intersection + self.smooth) / (cardinality + self.smooth)
+            return 1. - dice_score
+
+    def _binary_class(self, input, target, mask=None):
+        flat_input = input.view(-1)
+        flat_target = target.view(-1).float()
+        flat_input = torch.sigmoid(flat_input) if self.with_logits else flat_input
+
+        if mask is not None:
+            mask = mask.float()
+            flat_input = flat_input * mask
+            flat_target = flat_target * mask
+        else:
+            mask = torch.ones_like(target)
+
+        if self.ohem_ratio > 0:
+            pos_example = target > 0.5
+            neg_example = target <= 0.5
+            mask_neg_num = mask <= 0.5
+
+            pos_num = pos_example.sum() - (pos_example & mask_neg_num).sum()
+            neg_num = neg_example.sum()
+            keep_num = min(int(pos_num * self.ohem_ratio), neg_num)
+
+            neg_scores = torch.masked_select(flat_input, neg_example.bool())
+            neg_scores_sort, _ = torch.sort(neg_scores, )
+            threshold = neg_scores_sort[-keep_num+1]
+            cond = (flat_input > threshold) | pos_example.view(-1)
+            ohem_mask = torch.where(cond, 1, 0)
+            flat_input = flat_input * ohem_mask
+            flat_target = flat_target * ohem_mask
+
+        return self._compute_dice_loss(flat_input, flat_target)
+
+    def __str__(self):
+        return f"Dice Loss smooth:{self.smooth}, ohem: {self.ohem_ratio}, alpha: {self.alpha}"
+
+    def __repr__(self):
+        return str(self)
+
+
+
+
+
+
+
+class FocalLoss(nn.Module):
+    r"""Criterion that computes Focal loss.
+
+    According to [1], the Focal loss is computed as follows:
+
+    .. math::
+
+        \text{FL}(p_t) = -\alpha_t (1 - p_t)^{\gamma} \, \text{log}(p_t)
+
+    where:
+       - :math:`p_t` is the model's estimated probability for each class.
+
+
+    Arguments:
+        alpha (float): Weighting factor :math:`\alpha \in [0, 1]`.
+        gamma (float): Focusing parameter :math:`\gamma >= 0`.
+        reduction (Optional[str]): Specifies the reduction to apply to the
+         output: ‘none’ | ‘mean’ | ‘sum’. ‘none’: no reduction will be applied,
+         ‘mean’: the sum of the output will be divided by the number of elements
+         in the output, ‘sum’: the output will be summed. Default: ‘none’.
+
+    Shape:
+        - Input: :math:`(N, C, H, W)` where C = number of classes.
+        - Target: :math:`(N, H, W)` where each value is
+          :math:`0 ≤ targets[i] ≤ C−1`.
+
+    Examples:
+        >>> N = 5  # num_classes
+        >>> loss = tgm.losses.FocalLoss(alpha=0.5, gamma=2.0, reduction='mean')
+        >>> input = torch.randn(1, N, 3, 5, requires_grad=True)
+        >>> target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+        >>> output = loss(input, target)
+        >>> output.backward()
+
+    References:
+        [1] https://arxiv.org/abs/1708.02002
+    """
+
+    def __init__(self, alpha: float, gamma: Optional[float] = 2.0,
+                 reduction: Optional[str] = 'none', ignore_index: Optional[int] = -100,
+                 eps: Optional[float] = 1e-6) -> None:
+        super(FocalLoss, self).__init__()
+        self.alpha: float = alpha
+        self.gamma: Optional[float] = gamma
+        self.reduction: Optional[str] = reduction
+        self.eps: float = eps
+        self.ignore_index = ignore_index
+
+    def forward(
+            self,
+            input: torch.Tensor,
+            target: torch.Tensor) -> torch.Tensor:
+        if not torch.is_tensor(input):
+            raise TypeError("Input type is not a torch.Tensor. Got {}"
+                            .format(type(input)))
+        if len(input.shape) == 4:
+            #raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
+            #                 .format(input.shape))
+            if input.shape[-2:] != target.shape[-2:]:
+                raise ValueError("input and target shapes must have same 2 last dimensions. Got: {}"
+                             .format(input.shape, input.shape))
+        elif len(input.shape) == 2:
+            if input.shape[0] != target.shape[0]:
+                raise ValueError("number of elements in input and target shapes must be the same. Got: {}"
+                                 .format(input.shape, input.shape))
+        else:
+            raise ValueError("Invalid input shape, we expect BxNxHxW or BxN. Got: {}"
+                             .format(input.shape))
+        if not input.device == target.device:
+            raise ValueError(
+                "input and target must be in the same device. Got: {}" .format(
+                    input.device, target.device))
+        # compute softmax over the classes axis
+        input_soft = F.softmax(input, dim=1) + self.eps
+
+        # filter labels
+        target = target.type(torch.long)
+        input_mask = target != self.ignore_index
+
+        target = target[input_mask]
+        input_soft = input_soft[input_mask]
+
+        # create the labels one hot tensor
+        target_one_hot = F.one_hot(target, num_classes=input.shape[1])\
+            .to(input.device).float()
+
+        # compute the actual focal loss
+        weight = torch.pow(1. - input_soft, self.gamma)
+        focal = -self.alpha * weight * torch.log(input_soft)
+        loss_tmp = torch.sum(target_one_hot * focal, dim=1)
+
+        loss = -1
+        if self.reduction == 'none':
+            loss = loss_tmp
+        elif self.reduction == 'mean':
+            loss = torch.mean(loss_tmp)
+        elif self.reduction == 'sum':
+            loss = torch.sum(loss_tmp)
+        else:
+            raise NotImplementedError("Invalid reduction mode: {}"
+                                      .format(self.reduction))
+        return loss
